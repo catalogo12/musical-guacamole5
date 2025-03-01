76d7624c44c9571e217ec50117bf84b998680a38
catalogo12 <96148687+catalogo12@users.noreply.github.com>
2025-02-28 19:06:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express=require('express')
-const app=express()
-const port=3000
-app.get('/',(req,res)=>{
-    res.send("hoy 11 febrero 2025")
-})
+const express=require('express');
+const cors = require('cors');
+const app=express();
+const port=process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+//app.get('/',(req,res)=>{
+   // res.send("hoy 11 febrero 2025")
+//})
+
+app.post('/submit',(req,res)=>{
+    console.log(req,body);
+    res.send('detos recibidos');
+});
 app.listen(port,()=>{
-     console.log(`ejemplo app listen on port ${port}`)
-})
+     console.log(`ejemplo app listen on port ${port}`);
+});
+
